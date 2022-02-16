bc241cafc4da5a443ecb3aeaca3d5ce24d53d3ba
billydean <billy.goehring@gmail.com>
2022-02-16 12:23:10 -0800

    finished all components making api requests

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import axios from "axios";
 import APOD from "./components/Image";
 import Description from "./components/Description";
 import Meta from "./components/Meta";
-import Today from "./components/Today";
-import Random from "./components/Random";
 
+const Content = () => {
+    const [date, setDate] = useState("");
+
+const randomButton = () => {
+const randomDate = (start, end) => {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+};
+const sinceAPOD = randomDate(new Date(1995,5,16),new Date());
+setDate(sinceAPOD.toISOString().slice(0,10));
+}
+const resetButton = () => {
+    setDate("");
+}
+return (
+    <div>
+        <div className="top-content">
+            <APOD date={ date }/>
+            <Meta date={ date }/>
+        </div>
+        <Description />
+        <div className="buttons">
+            <button onClick={resetButton}>Today</button>
+            <button onClick={randomButton}>Random Day</button>
+        </div>
+    </div>
+)
+}
 export default Content;
