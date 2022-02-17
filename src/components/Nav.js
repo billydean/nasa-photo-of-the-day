0b193a108455ec67b1b995a2f507c8b431a4367f
billydean <billy.goehring@gmail.com>
2022-02-17 12:56:49 -0800

    hover effects on nav items

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,31 @@
 import React from "react";
 import styled from 'styled-components';
 
+const StyleNav = styled.nav`
+    display:flex;
+    width: 50%;
+    justify-content: space-evenly;
+    `;
+const Anchor = styled.a`
+    font-size: 3rem;
+    padding: 0.1rem 1rem;
+    border-radius: 10px;
+    text-decoration: none;
+    color: darkslategray;
+        &:hover{
+            transform: scale(1.4);
+            background-color: rgba(170,240,209,0.8);
+            transition: transform 750ms, background-color 250ms;
+        }
+    `;
+
 function Nav () {
     return (
-        <nav>
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
-        </nav>
+        <StyleNav>
+            <Anchor href="#">Home</Anchor>
+            <Anchor href="#">About</Anchor>
+            <Anchor href="#">Contact</Anchor>
+        </StyleNav>
     )
 }
 export default Nav;
